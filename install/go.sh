7a4491f24a1599eaaa13564812fa85d55aa77c42
Fabien Foerster <fabienfoerster@gmail.com>
2016-12-09 23:02:34 +0100

    Add go install script
    That doesn't really works but meeeeh

---
+++ install/go.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if test ! $(which gvm)
+then
+    echo "Installing a stable version of Go ..."
+    curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
+fi
+
+gvm install go1.4 --binary
+gvm install go1.7.4 --binary
+
+gvm use go1.7.4 --default
