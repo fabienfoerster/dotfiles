4bcfaab12e6a03984c596b4289a8a3e936faafa6
Fabien Foerster <fabienfoerster@gmail.com>
2016-12-09 21:01:02 +0100

    add install script for google-chrome

---
+++ install/chrome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if test ! $(which google-chrome)
+then
+	echo "Installing a stable version of Google Chrome ..."
+	apt-get install libxss1 libappindicator1 libindicator7
+	wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	dpkg -i /tmp/google-chrome*.deb
+fi
